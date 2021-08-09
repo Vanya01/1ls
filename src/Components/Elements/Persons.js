878f27eca2247257def8bd2e0d41cacc2b350adf
Vanya01 <good.boy.2k19@gmail.com>
2021-08-09 17:10:34 +0300

    class_work_react_1

--- src/Components/Elements/Persons.js
+++ src/Components/Elements/Persons.js
@@ -1,11 +1,16 @@
 import './Persons.css'
-export function Persons({title,picture,info,age}){
+export function Users({user,i}){
+
     return(
-        <div className={'person'}>
-            <h2>{title}</h2>
-            <h3>{age}</h3>
-            <img src={picture} alt="pgr"/>
-            <p>{info}</p>
+        <div className={'user'}>
+            <h2>{user[i].id} {user[i].name} {user[i].username}</h2>
+            <div>
+                <p>{user[i].address.street}</p>
+                <p>{user[i].address.suite}</p>
+                <p>{user[i].address.city}</p>
+                <p>{user[i].address.zipcode}</p>
+                <p>{user[i].address.geo.lat}</p>
+            </div>
         </div>
     )
 }
