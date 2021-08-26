e274e28be2784afeedafe584aaf865b5d15ebfe6
Vanya01 <good.boy.2k19@gmail.com>
2021-08-26 13:34:06 +0300

    Hw_ls7 - done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import './App.css';
 import {useReducer} from "react";
 
 
+const reducer = (state,action)=> {
 
-const reducer = (state={a:0,b:1},action)=> {
-
+      if (action.obj === '1' && action.act==='+'){
+          return (
+              {...state,a:state.a + 10}
+          )} else if (action.obj === '2' && action.act === '-'){
+          return (
+              {...state,a:state.a - 2}
+          )
+          }
 }
-function App() {
-    let [state,dispatch] = useReducer(reducer)
+
+
+
+export default function App() {
+    let [state,dispatch] = useReducer(reducer,{a: 0}); // here value of state is a=0 and b=0
+     let {a}=state
 
     return (
     <div className={'main_wrap'}>
-        <h3>State - {state1}</h3>
-       <button >Inc</button>
-        <h4>State - {state2}</h4>
-        <button >Dec</button>
+        <h3>State - {a} </h3>
+        <button  className={'btn1'} onClick={ () => { dispatch( {obj:'1', act:'+'} )}}>Sum</button>
+        <button className={'btn2'}  onClick={ () => { dispatch( {obj:'2', act:'-'} )}}>Minus</button>
+
+        <p> Here should be some text... Or smth else... </p>
     </div>
   );
 }
 
-export default App;
 
 
 // є об'єкт, значення якого 0
